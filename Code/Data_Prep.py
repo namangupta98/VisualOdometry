1027b8516c2723473219060b687660d05e0c3d1a
Naman Gupta <namanryan@gmail.com>
2020-04-23 18:13:20 -0400

    Added Data_Prep.py

--- Code/Data_Prep.py
+++ Code/Data_Prep.py
@@ -7,16 +7,10 @@ if __name__ == '__main__':
     # read dataset
     filenames = glob.glob("../Oxford_dataset/stereo/centre/*.png")
     filenames.sort()
-    photos = [cv2.imread(img) for img in filenames]
 
-    ctr = 0
-
-    for image in photos:
-
-        # show images
-        cv2.imshow('image', image)
-        ctr += 1
-        print(ctr)
+    for image in filenames:
+        frame = cv2.imread(image)
+        cv2.imshow('frame', frame)
 
         if cv2.waitKey(1) and 0xFF == ord('q'):
             break
